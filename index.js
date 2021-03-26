b654c328bcf4b7ccc3139dc4f7268fbcae86606a
Christian Nuss <christian.nuss@gmail.com>
2021-03-26 09:54:10 -0700

    Create terraform org and workspace

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const simpleGit = require("simple-git");
+const axios = require("axios");
 
-const getOrganization = async () => {
+const getOrgAndRepo = async () => {
   const remotes = await simpleGit.default().getRemotes(true);
   const origin = remotes.find((remote) => remote.name === "origin");
   if (!origin) {
@@ -14,33 +15,94 @@ const getOrganization = async () => {
     throw new Error(`Unable to extract pathname from ${origin.refs.push}`);
   }
 
-  const organization = pathname.split("/")[0];
+  const organization = pathname.split("/")[1];
   if (!organization) {
-    throw new Error(`Unable to extract organization from ${origin.refs.push}`);
+    throw new Error(`Unable to extract organization from ${pathname}`);
   }
-  return organization;
+
+  const repo = pathname.split("/")[2];
+  if (!repo) {
+    throw new Error(`Unable to extract repo from ${pathname}`);
+  }
+
+  return { organization, repo };
+};
+
+const terraformPost = async (url, data) => {
+  const terraformCloudToken = core.getInput("terraform-cloud-token");
+
+  const response = await axios.default.post(url, data, {
+    headers: {
+      Authorization: terraformCloudToken,
+      "Content-Type": "application/vnd.api+json",
+    },
+  });
+
+  return response;
+};
+
+const createTerraformOrganization = async (organization) => {
+  const rootEmail = core.getInput("root-email");
+
+  try {
+    const { status, data } = await terraformPost(
+      "https://app.terraform.io/api/v2/organizations",
+      {
+        data: {
+          type: "organizations",
+          attributes: {
+            name: organization,
+            email: rootEmail,
+          },
+        },
+      }
+    );
+    console.log(`Status Code: ${status}: Response: ${JSON.stringify(data)}`);
+  } catch (e) {
+    // TODO: Check message
+    console.warn("Error creating organization: ", e);
+  }
+};
+
+const createTerraformWorkspace = async (organization, workspace) => {
+  const rootEmail = core.getInput("root-email");
+
+  try {
+    const { status, data } = terraformPost(
+      `https://app.terraform.io/api/v2/organizations/${organization}/workspaces`,
+      {
+        data: {
+          type: "workspaces",
+          attributes: {
+            name: workspace,
+            operations: false,
+          },
+        },
+      }
+    );
+    console.log(`Status Code: ${status}: Response: ${JSON.stringify(data)}`);
+  } catch (e) {
+    // TODO: Check message
+    console.warn("Error creating workspace: ", e);
+  }
 };
 
 const setup = async () => {
-  console.log("!! HELLO WORLD !!");
   const repoToken = core.getInput("repo-token");
-  const rootEmail = core.getInput("root-email");
-  const terraformCloudToken = core.getInput("terraform-cloud-token");
 
   // const { orgs: orgsApi } = github.getOctokit(repoToken);
 
-  const orgId = await getOrganization();
-  console.log("!!!! orgId", orgId);
+  const { organization, repo } = await getOrgAndRepo();
+  core.setOutput("organization", organization);
 
-  // TODO: Terraform Cloud init
-
-  core.setOutput("organization", orgId);
+  await createTerraformOrganization(organization);
+  await createTerraformWorkspace(organization, repo);
 };
 
 (async () => {
   try {
     await setup();
-  } catch (error) {
-    core.setFailed(error.message);
+  } catch (e) {
+    core.setFailed(e.message);
   }
 })();
