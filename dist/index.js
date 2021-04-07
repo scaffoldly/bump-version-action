f7931e7d0f2ef5fe3629e947e640ef75b36123bc
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2021-04-07 14:21:38 +0000

    ci: version bump to v1.0.48

--- dist/index.js
+++ dist/index.js
@@ -304,7 +304,7 @@ const terraformPlan = async (planfile) => {
 };
 
 const terraformApply = async (org, repo, planfile) => {
-  let version = semver.inc(slyVersionFetch(), "patch");
+  let version = semver.parse(semver.inc(slyVersionFetch(), "patch"));
 
   //TODO Decrypt
   let output;
