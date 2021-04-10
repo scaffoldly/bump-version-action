5a09df13d18ab0a7b88a5b3f6bde093ced14580a
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2021-04-10 04:36:59 +0000

    ci: version bump to v1.0.67

--- dist/index.js
+++ dist/index.js
@@ -376,7 +376,6 @@ const terraformApply = async (org, repo, planfile) => {
 
   let version = semver.parse(semver.inc(slyVersionFetch(), "patch"));
 
-  //TODO Decrypt
   let output;
   try {
     const command = `terraform apply -no-color plan`;
